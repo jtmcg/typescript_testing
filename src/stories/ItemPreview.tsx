408d5368b4e1ea929f253d612aa447cf7e061324
Tyler Mcgoffin <jtmcgoffin@gmail.com>
2021-02-11 14:21:57 -0800

    Styled Grid view. WIP

--- src/stories/ItemPreview.tsx
+++ src/stories/ItemPreview.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import User from '../lib/user';
+import './itemPreview.css'
 
 export interface ItemPreviewProps {
     image: string;
@@ -9,21 +10,21 @@ export interface ItemPreviewProps {
 }
 
 export const ItemPreview: React.FC<ItemPreviewProps> = ({ image, itemName, itemDescription, user }) => (
-    <div className="itemPreview__container">
-        <div className="itemPreview__image-container">
-            <img className="itemPreview__image" src={image} alt="item preview" />
-            {/* <div className="itemPreview__image-details-container">
+    <div className={"itemPreview__container"}>
+        <div className={"itemPreview__image-container"}>
+            <img className={"itemPreview__image"} src={image} alt={"item preview"} />
+            <div className={"itemPreview__image-details-container"}>
                 <h2>{itemName}</h2>
                 <p>{itemDescription}</p>
-            </div> */}
-        </div>
-        <div className="itemPreview__details-container">
-            <div className="itemPreview__details-user">
-                <img className="itemPreview__details-user_image" src={user.getImageURL('thumbnail')} alt={'user thumb'} />
-                <h4>{user.name}</h4>
             </div>
-            <div className="itemPreview__details-location">
-                <h4>{user.generalLocation}</h4>
+        </div>
+        <div className={"itemPreview__details-container"}>
+            <div className={"itemPreview__details-user"}>
+                <img className={"itemPreview__details-user_image"} src={user.getImageURL('thumbnail')} alt={'user thumb'} />
+                <p>{user.name}</p>
+            </div>
+            <div className={"itemPreview__details-location"}>
+                <p>{user.generalLocation}</p>
             </div>
         </div>
     </div>
